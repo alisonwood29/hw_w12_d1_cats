3f578c4987911a1ec7dad294ecec2f1f15612191
Alison Wood <alisonwood29@gmail.com>
2018-02-19 20:38:14 +0000

    refactored to add all cats via an array rather than in the index.html

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
+const CATS_ARRAY = [
+  {name: "Boba", favFood: "Sock fluff", image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {name: "Barnaby", favFood: "Tuna", image: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  {name: "Max", favFood: "Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
+  {name: "Boots", favFood: "cat food", image: "http://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat.jpg"}
+]
+
 const addCat = function (name, favFood, image){
 
   const catListItem = document.createElement("ul");
@@ -9,14 +16,9 @@ const addCat = function (name, favFood, image){
   const favFoodListItem = document.createElement("li");
   favFoodListItem.innerText = "Favourite food: " + favFood;
 
-  // const imageItem = document.createElement("img");
-  // imageItem.width = 500;
-  // imageItem.src = image;
-
   const imageListItem = document.createElement("li");
   imageListItem.innerHTML = '<img width="500" src="'+ image + '">';
 
-  // imageItem.appendChild(imageListItem);
   favFoodListItem.appendChild(imageListItem);
   nameListItem.appendChild(favFoodListItem);
   catListItem.appendChild(nameListItem);
@@ -26,7 +28,11 @@ const addCat = function (name, favFood, image){
 };
 
 const app = function () {
-  addCat("Boots", "cat food", "http://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat.jpg")
+  // addCat("Boots", "cat food", "http://www.petsworld.in/blog/wp-content/uploads/2014/09/adorable-cat.jpg")
+  CATS_ARRAY.forEach(function (cat) {
+    addCat(cat.name, cat.favFood, cat.image);
+  })
+
 }
 
 document.addEventListener("DOMContentLoaded", app);
